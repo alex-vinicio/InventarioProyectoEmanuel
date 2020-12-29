6a76b3bb62ef1e78bd1b10d1a3fc7d256d1c3ae7
alex-vinicio <38170743+alex-vinicio@users.noreply.github.com>
2020-12-29 03:00:49 -0500

    Update funciones.js
    buscar

--- public/assets/js/inventario/funciones.js
+++ public/assets/js/inventario/funciones.js
@@ -23,14 +23,31 @@ function templateTitleCasaHogar(table){
     <thead> 
         <tr>
             <th><big>N°</big></th>
-            <th>Codigo</th>
-            <th>Descripcion</th>
+            <th>
+                <div >  
+                    <form name="searchCodProceden">
+                        <input type="text" class="form-control" name="codProduct" placeholder="Codigo:">
+                    </form>
+                </div>
+            </th>
+            <th>
+                <div >  
+                    <form name="searchNombre">
+                        <input type="text" class="form-control" name="nombreProduct" placeholder="Nombre:">
+                    </form>
+                </div>
+            </th>
             <th>Cantidad</th>
             <th>Medida</th>
-            <th>Unidad</th>
             <th>Fecha ingreso</th>
             <th>Fecha caducidad</th>
-            <th>procedencia</th>
+            <th>
+                <div  >
+                    <form name="searchProcedencia">
+                        <input type="text" class="form-control" name="ProcedenciaProd" placeholder="Procedencia:">
+                    </form>
+                </div>
+            </th>
             <th>Obervaciones</th>
             <th>Proyecto</th>
             <th>Grupo</th>
@@ -97,4 +114,85 @@ async function deleteProducto(id){
     }else{
         alertify.error('Error al eliminar el producto')
     }
+}
+//buscadores
+async function addEventSearchCodProcedencia(){
+    const formcod = document.searchCodProceden
+    const formProcedencia = document.searchProcedencia
+    const formNombre = document.searchNombre
+    formcod.codProduct.value = $formHidenProducto.codProducto.value
+    formProcedencia.ProcedenciaProd.value = $formHidenProducto.procedenciaProducto.value
+    formNombre.nombreProduct.value = $formHidenProducto.nombreProducto.value
+    //$divListInventary.innerHTML=""
+    formcod.addEventListener('submit', async (event)=>{
+        event.preventDefault()//recarga solo la tabla, no la pagina
+        const datos = dataInputProduct(formcod,formProcedencia,formNombre)// asigna a una lista valores para busqueda de nombre, procedencia y codigo
+        const listaProducto = await getData('getCasaHogar')
+        const list = await getFilterProduct(listaProducto, datos)// genera una lista de los valores encontrados 
+        await listaProductoTabla(list, $divListInventary) //actualiza tabla con datos encontrados
+    })
+    formProcedencia.addEventListener('submit', async (event)=>{
+        event.preventDefault()
+        const datos = dataInputProduct(formcod,formProcedencia,formNombre)
+        const listaProducto = await getData('getCasaHogar')
+        const list = await getFilterProduct(listaProducto, datos)
+        await listaProductoTabla(list,$divListInventary)
+    })
+    formNombre.addEventListener('submit', async (event)=>{
+        event.preventDefault()
+        const datos = dataInputProduct(formcod,formProcedencia,formNombre)
+        const listaProducto = await getData('getCasaHogar')
+        const list = await getFilterProduct(listaProducto, datos)
+        await listaProductoTabla(list,$divListInventary)
+    })
+} 
+function dataInputProduct(formcod,formProcedencia, formNombre){
+    let data = []
+    const codigo = formcod.elements.codProduct.value
+    $formHidenProducto.codProducto.value = codigo
+    if(codigo != ""){data.push({name:'codigo',val:codigo})} //inserta en lista valores para buscar si no son vacios
+   
+    const procedencia = formProcedencia.elements.ProcedenciaProd.value
+    $formHidenProducto.procedenciaProducto.value = procedencia
+    if(procedencia != ""){data.push({name:'procedencia',val:procedencia})}
+
+    const nombre = formNombre.elements.nombreProduct.value
+    $formHidenProducto.nombreProducto.value = nombre
+    if(nombre != ""){data.push({name:'nombre',val:nombre})}
+
+    return data
+}
+async function getFilterProduct(list,data){
+    if(data.length == 0){return list }//si no ingresa ningun valor a buscar
+
+    let codigoP = procedenciaP = nombre = null //ahorar codigo asignando null a multiples variables
+    let lista = []
+    for(var i = 0; i< data.length;i++){//recorre lista de valores antes guardados y asigna a listas independientes
+        if(data[i].name === 'codigo'){
+            codigoP = data[i].val
+        }else{
+            if(data[i].name === 'procedencia'){
+                procedenciaP = data[i].val
+            }else{
+                nombre = data[i].val
+            }
+        }
+    }   
+    switch(data.length){//manejo de 2 casos
+        case 2: //caso si ingreso valores en todos los inputs
+            list.forEach((lt)=>{ if(lt.codigo.indexOf(codigoP) !== -1 || lt.procedencia.indexOf(procedenciaP) !== -1){ lista.push(lt) } })//guarda en lista si encontro el nombre y estado
+            break
+        case 1: //caso si solo ingreso valor en nombre o estado
+            if(codigoP != null){
+                list.forEach((lt)=>{ if(lt.codigo.indexOf(codigoP) !== -1){ lista.push(lt) } }) 
+            }else{
+                if(procedenciaP != null){
+                    list.forEach((lt)=>{ if(lt.procedencia.indexOf(procedenciaP) !== -1){ lista.push(lt) } }) 
+                }else{
+                    list.forEach((lt)=>{ if(lt.descripcionProducto.indexOf(nombre) !== -1){ lista.push(lt) } })    
+                }        
+            }
+            break   
+    }
+    return lista
 }
