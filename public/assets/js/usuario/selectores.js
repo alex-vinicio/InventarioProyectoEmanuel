3ddd7a477834c68b3a5c118cba8722d5c4a0f5f4
aleax <alexandino2011@gmail.com>
2021-09-21 19:20:28 -0500

    test cache login v4

--- public/assets/js/usuario/selectores.js
+++ public/assets/js/usuario/selectores.js
@@ -4,12 +4,6 @@ const $divProxCad = document.getElementById('productos_proximos_caducar');
 //listado automatico
 (async function load(){
     //controlLogin
-    const data = new URLSearchParams(`peticion="ok"`)
-    const response = await getDataPost('verificacionIpCliente', data)
-    console.log(response)
-    if(response !== "ok"){
-        location.href="/";
-    }
     await getTableProxCaducar($divProxCad)
     await userNavigation($divNameUser)
 })()
